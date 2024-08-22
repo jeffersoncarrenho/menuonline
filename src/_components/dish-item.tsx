c127b5bd23c827223fb284a2e1eea6e2443b5cb1
Jefferson Lima <jeferson.carrenho@gmail.com>
2024-08-21 21:13:21 -0300

    fix: single dish type errors

--- src/_components/dish-item.tsx
+++ src/_components/dish-item.tsx
@@ -15,17 +15,17 @@ interface DishItemProps {
 
 const DishItem = ({ dish }: DishItemProps) => {
     return (
-        <Link href={`/dish/${dish.id}`}>
+        <Link href={`/dish/${dish?.id}`}>
             <Card className="flex items-center justify-start rounded-xl gap-3 my-3 p-3">
                 <div>
-                    <Image src={dish.image!} width={100} height={70} className="rounded-lg object-cover" alt={dish.name!} />
+                    <Image src={dish?.image!} width={100} height={70} className="rounded-lg object-cover" alt={dish?.name!} />
                 </div>
                 <div>
-                    <h3 className="text-[14px]">{dish.name}</h3>
+                    <h3 className="text-[14px]">{dish?.name}</h3>
                     <p className="text-[10px] font-light">
-                        {dish.description?.substring(0, 35)}
+                        {dish.description?.substring(0, 35)!}
                     </p>
-                    <p>${dish.price}</p>
+                    <p>${dish?.price!}</p>
                 </div>
             </Card>
         </Link>
