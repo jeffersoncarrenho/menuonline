72fb82e687afdedac022f4a8677bf0b648772f7a
Jefferson Lima <jeferson.carrenho@gmail.com>
2024-08-21 21:16:46 -0300

    fix: home-categories

--- src/_components/home-categories.tsx
+++ src/_components/home-categories.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import DishItem from "./dish-item";
 import { db } from "@/_lib/prisma";
 
 interface HomeCategoriesProps {
@@ -20,22 +19,12 @@ const HomeCategories = async ({ category }: HomeCategoriesProps) => {
 
     return (
         <>
-            {dishes.length > 1 && (
-                <>
-                    <div className="flex justify-between mt-5 mb-2">
-                        <h2 className="font-bold">{category.name}</h2>
-                        <Link href={`/category/${category.id}`}>
-                            <p className="text-xs">Ver todos</p>
-                        </Link>
-                    </div>
-                    <div className="my-5">
-                        {dishes?.map(dish =>
-                            <DishItem dish={dish} key={dish.id} />
-                        ).slice(0, 3)}
-                    </div>
-                </>
-            )}
-
+            <div className="flex justify-between mt-5 mb-2">
+                <h2 className="font-bold">{category.name}</h2>
+                <Link href={`/category/${category.id}`}>
+                    <p className="text-xs">Ver todos</p>
+                </Link>
+            </div>
         </>
     );
 }
