2658cdc9859224b1e199bdc67006e61d6187b299
Jefferson Lima <jeferson.carrenho@gmail.com>
2024-08-22 16:18:51 -0300

    test: Home page

--- src/_lib/data.ts
+++ src/_lib/data.ts
@@ -1,5 +1,12 @@
 import { db } from "./prisma";
 
+
+export async function getRestaurants() {
+    const restaurants = await db.restaurant.findMany();
+    return restaurants;
+}
+
+
 export async function getRestaurantById(restaurantId: string) {
     try {
         const restaurant = await db.restaurant.findUnique({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,26 @@ import { categories, dishes } from "@/_constants/categories"
 import HomeCategories from "@/_components/home-categories";
 import Header from "@/_components/header";
 import CategoriesSlider from "@/_components/categories-slider";
+import { getRestaurants } from "@/_lib/data";
+import { Card } from "@/_components/ui/card";
+import Image from "next/image";
 
-export default function Home() {
+
+
+export default async function Home() {
+  const restaurants = await getRestaurants()
+  console.log(restaurants)
 
   return (
     <main>
 
-      <div className="flex h-full w-full items-center justify-center">
+      <div className="flex flex-col h-full w-full items-center justify-center">
         <h1>Jump Foods</h1>
+        {restaurants.map(restaurant => (
+          <Card>
+            <Image src={restaurant?.image!} width={100} height={100} alt={restaurant?.name!} />
+          </Card>
+        ))}
       </div>
 
     </main>
